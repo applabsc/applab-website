a636bbdb3ad28fedeb71c78c072739b5635e2e92
Christopher Wang <wang967@usc.edu>
2018-11-12 17:54:18 -0800

    header implement

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import React, {Component} from 'react';
+import "../Styles/Header.scss";
 
-export default Header
+class Header extends Component {
+
+    render() {
+        return (
+            <div className={"sticky-header"}>
+                <div className={'title'}>
+                    <h1>Applab USC</h1>
+                </div>
+
+                <div className={'links'}>
+                    <div style={styles.headerItem}>
+                        <Link to="/" style={styles.link}>
+                            Link 1
+                        </Link>
+                    </div>
+
+                    <div style={styles.headerItem}>
+                        <Link to="/" style={styles.link}>
+                            Link 2
+                        </Link>
+                    </div>
+
+                    <div style={styles.headerItem}>
+                        <Link to="/" style={styles.link}>
+                            Link 3
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const styles = {
+    headerItem: {
+        display: 'flex',
+    },
+    link: {
+        color: 'white',
+        textDecoration: 'none',
+    },
+};
+
+export default Header;
