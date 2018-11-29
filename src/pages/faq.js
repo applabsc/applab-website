aa1b39e992be0ba311f45e234fd6e959172e0bee
Christopher Wang <wang967@usc.edu>
2018-11-28 17:24:11 -0800

    changes n additions

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,40 +1,71 @@
 import React, {Component} from 'react'
 import {StaticQuery} from '../../.cache/gatsby-browser-entry';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import {FiChevronUp} from 'react-icons/fi';
+import {FiChevronDown} from 'react-icons/fi';
 
-const questionAnswers = [
-    {
-        questionText: "When are the meeting times?",
-        answer: "Wednesdays 8:00 to 9:30pm",
-    },
-    {
-        questionText: "What's the expected time commitment?",
-        answer: "Besides the 1.5 hours for weekly meetings, expect to put in around 2 hours outside the meeting.",
-    },
-    {
-        questionText: "Do I have to reapply every semester to remain a member of Applab?",
-        answer: "Nope! Once you get in, you can remain a member as long as you want.",
-    },
-    {
-        questionText: "Are there any costs associated with being a member of Applab?",
-        answer: "Dues are $40 per semester.",
+
+
+
+class FrequentlyAskedQuestions extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            questions: {
+                q1: {
+                    questionText: "When are the meeting times?",
+                    answer: "Wednesdays 8:00 to 9:30pm",
+                    open: false,
+                },
+                q2: {
+                    questionText: "What's the expected time commitment?",
+                    answer: "Besides the 1.5 hours for weekly meetings, expect to put in around 2 hours outside the meeting.",
+                    open: false,
+                },
+                q3: {
+                    questionText: "Do I have to reapply every semester to remain a member of Applab?",
+                    answer: "Nope! Once you get in, you can remain a member as long as you want.",
+                    open: false,
+                },
+                q4: {
+                    questionText: "Are there any costs associated with being a member of Applab?",
+                    answer: "Dues are $40 per semester.",
+                    open: false,
+                }
+            }
+        }
     }
-];
 
-const renderQuestion = (question) => {
-    return (
-        <div className={'questionContainer'}>
-            <div>{question}</div>
-            <FiChevronUp/>
-        </div>
-    )
-};
 
-const FrequentlyAskedQuestions = () => (
-    <ExpansionPanel
-        children={renderQuestion("erjbiu")}
-    />
-);
+    render() {
+        const questions = this.state.questions;
+
+        return Object.keys(questions).map(key => {
+            const triggerComponent =
+                <FAQQuestion
+                    question={questions[key].questionText}
+                    opened={questions[key].open}
+                />;
+
+            return <Collapsible
+                trigger={triggerComponent}
+                onOpening={() => this.onTrigger(key)}
+                onClosing={() => this.onTrigger(key)}
+            >
+                <p>{questions[key].answer}</p>
+            </Collapsible>
+        })
+    }
+}
 
 export default FrequentlyAskedQuestions;
+
+const styles = {
+    answer: {
+        backgroundColor: 'cyan',
+        transition: 'height 400ms ease-in-out',
+    },
+    expansionPanelDetails: {
+        paddingVertical: 0,
+        marginVertical: 0,
+    }
+};
